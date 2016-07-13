24fdc2840d206fcf34670d6d3324149929d5a3e6
ikuto0608 <ikuto0608@me.com>
2016-07-13 12:59:49 -0700

    modify

---
+++ vb_itd/SortArray.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SortArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAddGrade = New System.Windows.Forms.Button()
+        Me.btnSortGrade = New System.Windows.Forms.Button()
+        Me.tbxGrade = New System.Windows.Forms.TextBox()
+        Me.lbxGrade = New System.Windows.Forms.ListBox()
+        Me.lbxSort = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbxRandom = New System.Windows.Forms.ListBox()
+        Me.lbxAscSort = New System.Windows.Forms.ListBox()
+        Me.lbxDscSort = New System.Windows.Forms.ListBox()
+        Me.btnRandom = New System.Windows.Forms.Button()
+        Me.btnAscSort = New System.Windows.Forms.Button()
+        Me.btnDesc = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.tbxNo = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnDimention = New System.Windows.Forms.Button()
+        Me.lbxDimention = New System.Windows.Forms.ListBox()
+        Me.lbxResult = New System.Windows.Forms.ListBox()
+        Me.lbxTwo = New System.Windows.Forms.ListBox()
+        Me.lbxOne = New System.Windows.Forms.ListBox()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAddGrade
+        '
+        Me.btnAddGrade.BackColor = System.Drawing.Color.Pink
+        Me.btnAddGrade.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnAddGrade.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAddGrade.ForeColor = System.Drawing.Color.White
+        Me.btnAddGrade.Location = New System.Drawing.Point(161, 58)
+        Me.btnAddGrade.Name = "btnAddGrade"
+        Me.btnAddGrade.Size = New System.Drawing.Size(100, 23)
+        Me.btnAddGrade.TabIndex = 1
+        Me.btnAddGrade.Text = "Add Grade"
+        Me.btnAddGrade.UseVisualStyleBackColor = False
+        '
+        'btnSortGrade
+        '
+        Me.btnSortGrade.BackColor = System.Drawing.Color.Pink
+        Me.btnSortGrade.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnSortGrade.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSortGrade.ForeColor = System.Drawing.Color.White
+        Me.btnSortGrade.Location = New System.Drawing.Point(161, 95)
+        Me.btnSortGrade.Name = "btnSortGrade"
+        Me.btnSortGrade.Size = New System.Drawing.Size(100, 23)
+        Me.btnSortGrade.TabIndex = 2
+        Me.btnSortGrade.Text = "Sort Grade"
+        Me.btnSortGrade.UseVisualStyleBackColor = False
+        '
+        'tbxGrade
+        '
+        Me.tbxGrade.BackColor = System.Drawing.Color.LimeGreen
+        Me.tbxGrade.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.tbxGrade.ForeColor = System.Drawing.Color.White
+        Me.tbxGrade.Location = New System.Drawing.Point(161, 23)
+        Me.tbxGrade.Name = "tbxGrade"
+        Me.tbxGrade.Size = New System.Drawing.Size(100, 13)
+        Me.tbxGrade.TabIndex = 3
+        '
+        'lbxGrade
+        '
+        Me.lbxGrade.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxGrade.FormattingEnabled = True
+        Me.lbxGrade.Location = New System.Drawing.Point(12, 23)
+        Me.lbxGrade.Name = "lbxGrade"
+        Me.lbxGrade.Size = New System.Drawing.Size(120, 91)
+        Me.lbxGrade.TabIndex = 4
+        '
+        'lbxSort
+        '
+        Me.lbxSort.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxSort.FormattingEnabled = True
+        Me.lbxSort.Location = New System.Drawing.Point(12, 154)
+        Me.lbxSort.Name = "lbxSort"
+        Me.lbxSort.Size = New System.Drawing.Size(120, 91)
+        Me.lbxSort.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Grade List"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Sort Grade List"
+        '
+        'lbxRandom
+        '
+        Me.lbxRandom.BackColor = System.Drawing.Color.Pink
+        Me.lbxRandom.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxRandom.ForeColor = System.Drawing.Color.White
+        Me.lbxRandom.FormattingEnabled = True
+        Me.lbxRandom.Location = New System.Drawing.Point(282, 23)
+        Me.lbxRandom.Name = "lbxRandom"
+        Me.lbxRandom.Size = New System.Drawing.Size(42, 221)
+        Me.lbxRandom.TabIndex = 8
+        '
+        'lbxAscSort
+        '
+        Me.lbxAscSort.BackColor = System.Drawing.Color.Pink
+        Me.lbxAscSort.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxAscSort.ForeColor = System.Drawing.Color.White
+        Me.lbxAscSort.FormattingEnabled = True
+        Me.lbxAscSort.Location = New System.Drawing.Point(353, 23)
+        Me.lbxAscSort.Name = "lbxAscSort"
+        Me.lbxAscSort.Size = New System.Drawing.Size(42, 221)
+        Me.lbxAscSort.TabIndex = 9
+        '
+        'lbxDscSort
+        '
+        Me.lbxDscSort.BackColor = System.Drawing.Color.Pink
+        Me.lbxDscSort.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxDscSort.ForeColor = System.Drawing.Color.White
+        Me.lbxDscSort.FormattingEnabled = True
+        Me.lbxDscSort.Location = New System.Drawing.Point(423, 23)
+        Me.lbxDscSort.Name = "lbxDscSort"
+        Me.lbxDscSort.Size = New System.Drawing.Size(42, 221)
+        Me.lbxDscSort.TabIndex = 10
+        '
+        'btnRandom
+        '
+        Me.btnRandom.BackColor = System.Drawing.Color.Pink
+        Me.btnRandom.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnRandom.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRandom.ForeColor = System.Drawing.Color.White
+        Me.btnRandom.Location = New System.Drawing.Point(161, 154)
+        Me.btnRandom.Name = "btnRandom"
+        Me.btnRandom.Size = New System.Drawing.Size(100, 23)
+        Me.btnRandom.TabIndex = 11
+        Me.btnRandom.Text = "Random"
+        Me.btnRandom.UseVisualStyleBackColor = False
+        '
+        'btnAscSort
+        '
+        Me.btnAscSort.BackColor = System.Drawing.Color.Pink
+        Me.btnAscSort.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnAscSort.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAscSort.ForeColor = System.Drawing.Color.White
+        Me.btnAscSort.Location = New System.Drawing.Point(161, 188)
+        Me.btnAscSort.Name = "btnAscSort"
+        Me.btnAscSort.Size = New System.Drawing.Size(100, 23)
+        Me.btnAscSort.TabIndex = 12
+        Me.btnAscSort.Text = "Sort Asc"
+        Me.btnAscSort.UseVisualStyleBackColor = False
+        '
+        'btnDesc
+        '
+        Me.btnDesc.BackColor = System.Drawing.Color.Pink
+        Me.btnDesc.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnDesc.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDesc.ForeColor = System.Drawing.Color.White
+        Me.btnDesc.Location = New System.Drawing.Point(161, 226)
+        Me.btnDesc.Name = "btnDesc"
+        Me.btnDesc.Size = New System.Drawing.Size(100, 23)
+        Me.btnDesc.TabIndex = 13
+        Me.btnDesc.Text = "Sort Desk"
+        Me.btnDesc.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(279, 5)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Random"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(350, 5)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(25, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Asc"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(420, 5)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Desc"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.BackColor = System.Drawing.Color.Pink
+        Me.btnSearch.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSearch.ForeColor = System.Drawing.Color.White
+        Me.btnSearch.Location = New System.Drawing.Point(487, 58)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(100, 23)
+        Me.btnSearch.TabIndex = 17
+        Me.btnSearch.Text = "Self Search"
+        Me.btnSearch.UseVisualStyleBackColor = False
+        '
+        'tbxNo
+        '
+        Me.tbxNo.BackColor = System.Drawing.Color.LimeGreen
+        Me.tbxNo.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.tbxNo.ForeColor = System.Drawing.Color.White
+        Me.tbxNo.Location = New System.Drawing.Point(550, 23)
+        Me.tbxNo.Name = "tbxNo"
+        Me.tbxNo.Size = New System.Drawing.Size(37, 13)
+        Me.tbxNo.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(484, 23)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 13)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Search No"
+        '
+        'btnDimention
+        '
+        Me.btnDimention.BackColor = System.Drawing.Color.Pink
+        Me.btnDimention.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnDimention.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDimention.ForeColor = System.Drawing.Color.White
+        Me.btnDimention.Location = New System.Drawing.Point(487, 95)
+        Me.btnDimention.Name = "btnDimention"
+        Me.btnDimention.Size = New System.Drawing.Size(100, 23)
+        Me.btnDimention.TabIndex = 20
+        Me.btnDimention.Text = "Dimention"
+        Me.btnDimention.UseVisualStyleBackColor = False
+        '
+        'lbxDimention
+        '
+        Me.lbxDimention.BackColor = System.Drawing.Color.Pink
+        Me.lbxDimention.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxDimention.ForeColor = System.Drawing.Color.White
+        Me.lbxDimention.FormattingEnabled = True
+        Me.lbxDimention.Location = New System.Drawing.Point(487, 138)
+        Me.lbxDimention.Name = "lbxDimention"
+        Me.lbxDimention.Size = New System.Drawing.Size(100, 104)
+        Me.lbxDimention.TabIndex = 21
+        '
+        'lbxResult
+        '
+        Me.lbxResult.BackColor = System.Drawing.Color.Pink
+        Me.lbxResult.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxResult.ForeColor = System.Drawing.Color.White
+        Me.lbxResult.FormattingEnabled = True
+        Me.lbxResult.Location = New System.Drawing.Point(865, 24)
+        Me.lbxResult.Name = "lbxResult"
+        Me.lbxResult.Size = New System.Drawing.Size(113, 117)
+        Me.lbxResult.TabIndex = 24
+        '
+        'lbxTwo
+        '
+        Me.lbxTwo.BackColor = System.Drawing.Color.Pink
+        Me.lbxTwo.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxTwo.ForeColor = System.Drawing.Color.White
+        Me.lbxTwo.FormattingEnabled = True
+        Me.lbxTwo.Location = New System.Drawing.Point(740, 23)
+        Me.lbxTwo.Name = "lbxTwo"
+        Me.lbxTwo.Size = New System.Drawing.Size(113, 117)
+        Me.lbxTwo.TabIndex = 23
+        '
+        'lbxOne
+        '
+        Me.lbxOne.BackColor = System.Drawing.Color.Pink
+        Me.lbxOne.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.lbxOne.ForeColor = System.Drawing.Color.White
+        Me.lbxOne.FormattingEnabled = True
+        Me.lbxOne.Location = New System.Drawing.Point(618, 23)
+        Me.lbxOne.Name = "lbxOne"
+        Me.lbxOne.Size = New System.Drawing.Size(113, 117)
+        Me.lbxOne.TabIndex = 22
+        '
+        'btnCreate
+        '
+        Me.btnCreate.BackColor = System.Drawing.Color.Pink
+        Me.btnCreate.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCreate.ForeColor = System.Drawing.Color.White
+        Me.btnCreate.Location = New System.Drawing.Point(618, 221)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(113, 23)
+        Me.btnCreate.TabIndex = 25
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = False
+        '
+        'btnCalc
+        '
+        Me.btnCalc.BackColor = System.Drawing.Color.Pink
+        Me.btnCalc.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnCalc.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCalc.ForeColor = System.Drawing.Color.White
+        Me.btnCalc.Location = New System.Drawing.Point(740, 221)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(113, 23)
+        Me.btnCalc.TabIndex = 26
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = False
+        '
+        'SortArray
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.AliceBlue
+        Me.ClientSize = New System.Drawing.Size(1010, 261)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.lbxResult)
+        Me.Controls.Add(Me.lbxTwo)
+        Me.Controls.Add(Me.lbxOne)
+        Me.Controls.Add(Me.lbxDimention)
+        Me.Controls.Add(Me.btnDimention)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.tbxNo)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnDesc)
+        Me.Controls.Add(Me.btnAscSort)
+        Me.Controls.Add(Me.btnRandom)
+        Me.Controls.Add(Me.lbxDscSort)
+        Me.Controls.Add(Me.lbxAscSort)
+        Me.Controls.Add(Me.lbxRandom)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbxSort)
+        Me.Controls.Add(Me.lbxGrade)
+        Me.Controls.Add(Me.tbxGrade)
+        Me.Controls.Add(Me.btnSortGrade)
+        Me.Controls.Add(Me.btnAddGrade)
+        Me.Name = "SortArray"
+        Me.Text = "SortArray"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAddGrade As System.Windows.Forms.Button
+    Friend WithEvents btnSortGrade As System.Windows.Forms.Button
+    Friend WithEvents tbxGrade As System.Windows.Forms.TextBox
+    Friend WithEvents lbxGrade As System.Windows.Forms.ListBox
+    Friend WithEvents lbxSort As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lbxRandom As System.Windows.Forms.ListBox
+    Friend WithEvents lbxAscSort As System.Windows.Forms.ListBox
+    Friend WithEvents lbxDscSort As System.Windows.Forms.ListBox
+    Friend WithEvents btnRandom As System.Windows.Forms.Button
+    Friend WithEvents btnAscSort As System.Windows.Forms.Button
+    Friend WithEvents btnDesc As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents tbxNo As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents btnDimention As System.Windows.Forms.Button
+    Friend WithEvents lbxDimention As System.Windows.Forms.ListBox
+    Friend WithEvents lbxResult As System.Windows.Forms.ListBox
+    Friend WithEvents lbxTwo As System.Windows.Forms.ListBox
+    Friend WithEvents lbxOne As System.Windows.Forms.ListBox
+    Friend WithEvents btnCreate As System.Windows.Forms.Button
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+End Class
