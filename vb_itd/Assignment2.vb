6f4a2c8c0e3333656abc1c6fb6bc9112c13cf2c4
ikuto0608 <ikuto0608@me.com>
2016-07-14 22:02:45 -0700

    modify assignment2

--- vb_itd/Assignment2.vb
+++ vb_itd/Assignment2.vb
@@ -1,17 +1,75 @@
 ﻿Public Class Assignment2
+    Private seatAlphabets As Array = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
+
     Private Sub Assignment2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim i As Integer
+        createBusinessSeatButtons()
+        createEconomySeatButtons()
 
-        For i = 1 To 10
-            Dim btn As New Button
-            btn.Width = 28
-            btn.Height = 28
-            btn.Location = New System.Drawing.Point(12, 24)
-            btn.Text = i
-            btn.Visible = True
-            btn.Name = i
+        picWhite.SendToBack()
+        picPlane.SendToBack()
 
-            Controls.Add(btn)
+    End Sub
+
+    Private Sub Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        'MsgBox(sender.Name)
+        sender.BackColor = Color.IndianRed
+    End Sub
+
+    Private Sub createBusinessSeatButtons()
+        Dim businessLocationsList As Array = {{172, 163}, {204, 163}, {253, 163}, {290, 163}, {383, 163}}
+
+        For x As Integer = 0 To businessLocationsList.GetUpperBound(0)
+            For y As Integer = 0 To 3
+                Dim btn As New Button
+                btn.Width = 28
+                btn.Height = 28
+                btn.Text = seatAlphabets(y)
+                btn.Visible = True
+                btn.FlatStyle = FlatStyle.Flat
+                btn.FlatAppearance.BorderColor = Color.White
+                btn.BackColor = Color.PowderBlue
+                btn.Name = seatAlphabets(y) & (x + 1)
+                btn.Location = New System.Drawing.Point(businessLocationsList(x, 0), businessLocationsList(x, 1) + (44 * y))
+                AddHandler btn.Click, AddressOf Me.Button_Click
+                Controls.Add(btn)
+            Next
+        Next
+
+    End Sub
+
+    Private Sub createEconomySeatButtons()
+        Dim economyLocationsList(29, 1) As Integer
+        Dim distanceOfEconomy As Integer = 17
+        Dim initLocation = {417, 160}
+        economyLocationsList(0, 0) = initLocation(0)
+        economyLocationsList(0, 1) = initLocation(1)
+
+        Dim index As Integer = 0
+        For x As Integer = 0 To 29
+            If index <> 0 Then
+                economyLocationsList(x, 0) = economyLocationsList(x - 1, 0) + distanceOfEconomy
+                economyLocationsList(x, 1) = economyLocationsList(x - 1, 1)
+            End If
+            index += 1
+        Next
+
+        For x As Integer = 0 To economyLocationsList.GetUpperBound(0)
+            For y As Integer = 0 To 9
+                Dim btn As New Button
+                btn.Width = 16
+                btn.Height = 16
+                btn.Font = New Font("Microsoft Sans Serif", 5)
+                btn.Text = seatAlphabets(y)
+                btn.Visible = True
+                btn.FlatStyle = FlatStyle.Flat
+                btn.FlatAppearance.BorderColor = Color.White
+                btn.BackColor = Color.PowderBlue
+                btn.Name = seatAlphabets(y) & (x + 1 + 5)
+                btn.Location = New System.Drawing.Point(economyLocationsList(x, 0), economyLocationsList(x, 1) + (16.5 * y))
+                AddHandler btn.Click, AddressOf Me.Button_Click
+                Controls.Add(btn)
+            Next
         Next
     End Sub
+
 End Class
